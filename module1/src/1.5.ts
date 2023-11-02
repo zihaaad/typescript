3be12a386ee29678c0e923ed402672031037a3e9
Zihadul Islam <zihadislam2429@gmail.com>
2023-11-02 10:58:09 +0600

    object, literal & function type

---
+++ module1/src/1.5.ts
@@ -0,0 +1,17 @@
+// Reference Type (Obkect)
+const user: {
+  readonly firstName: string;
+  middleName?: string; // optional
+  lastName: string;
+  age: number;
+  isMarried: boolean;
+  company: "Programming Hero"; // literal type
+} = {
+  firstName: "Zihadul",
+  lastName: "Islam",
+  age: 19,
+  isMarried: false,
+  company: "Programming Hero",
+};
+
+// user.firstName = "zihaaad";
