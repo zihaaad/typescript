b8c7381a5dae530811d30d562d4bf9b4be499de7
Zihadul Islam <zihadislam2429@gmail.com>
2023-11-04 10:52:55 +0600

    type assertion & interface vs type allias

---
+++ module2/2.1.ts
@@ -0,0 +1,35 @@
+{
+  // Type Assertion
+
+  let anything: any;
+
+  anything = "Next Level Web Devlopment";
+  anything = true;
+
+  anything = 222;
+  anything as number;
+
+  const kgToGm = (value: string | number): string | number | undefined => {
+    if (typeof value === "string") {
+      const convertedValue = parseFloat(value) * 1000;
+      return `The converted value is ${convertedValue}`;
+    }
+    if (typeof value === "number") {
+      return value * 1000;
+    }
+  };
+
+  const result1 = kgToGm(1000) as number;
+  const result2 = kgToGm("1000") as string;
+
+  type CustomError = {
+    message: string;
+  };
+
+  try {
+  } catch (error) {
+    console.log((error as CustomError).message);
+  }
+
+  //
+}
---
+++ module2/2.2.ts
@@ -0,0 +1,47 @@
+{
+  // Interface
+
+  type User1 = {
+    name: string;
+    age: number;
+  };
+
+  interface User2 {
+    name: string;
+    age: number;
+  }
+
+  type UserWithRole1 = User1 & {role: string};
+
+  interface UserWithRole2 extends User1 {
+    role: string;
+  }
+
+  const user1: UserWithRole2 = {
+    name: "Zihad",
+    role: "admin",
+    age: 19,
+  };
+
+  type RollNumber = number;
+
+  // js --> object , array -> object, funtion -> object
+
+  type Roll = number[];
+
+  interface Roll2 {
+    [index: number]: number;
+  }
+
+  const rollNumber1: Roll2 = [1, 2, 3];
+
+  type Add1 = (num1: number, num2: number) => number;
+
+  interface Add2 {
+    (num1: number, num2: number): number;
+  }
+
+  const add: Add2 = (num1, num2) => num1 + num2;
+
+  //
+}
